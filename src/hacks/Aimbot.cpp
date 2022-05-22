c18a6a70dbddb2d47852f228dfd75c79fb9bd295
STEVE4 <stephenmartinemail20030@gmail.com>
2022-05-22 06:29:02 -0400

    fixed

--- src/hacks/Aimbot.cpp
+++ src/hacks/Aimbot.cpp
@@ -22,7 +22,72 @@
 
 namespace hacks::shared::aimbot
 {
+static settings::Boolean normal_enable{ "aimbot.enable", "false" };
+static settings::Button aimkey{ "aimbot.aimkey.button", "<null>" };
+static settings::Int aimkey_mode{ "aimbot.aimkey.mode", "1" };
+static settings::Boolean autoshoot{ "aimbot.autoshoot", "1" };
+static settings::Boolean autoreload{ "aimbot.autoshoot.activate-heatmaker", "false" };
+static settings::Boolean autoshoot_disguised{ "aimbot.autoshoot-disguised", "1" };
+static settings::Int multipoint{ "aimbot.multipoint", "0" };
+static settings::Int hitbox_mode{ "aimbot.hitbox-mode", "0" };
+static settings::Float normal_fov{ "aimbot.fov", "0" };
+static settings::Int priority_mode{ "aimbot.priority-mode", "0" };
+static settings::Boolean wait_for_charge{ "aimbot.wait-for-charge", "0" };
 
+static settings::Boolean silent{ "aimbot.silent", "1" };
+static settings::Boolean target_lock{ "aimbot.lock-target", "0" };
+#if ENABLE_VISUALS
+static settings::Boolean assistance_only{ "aimbot.assistance.only", "0" };
+#endif
+static settings::Int hitbox{ "aimbot.hitbox", "0" };
+static settings::Boolean zoomed_only{ "aimbot.zoomed-only", "1" };
+static settings::Boolean only_can_shoot{ "aimbot.can-shoot-only", "1" };
+
+static settings::Boolean extrapolate{ "aimbot.extrapolate", "0" };
+static settings::Int normal_slow_aim{ "aimbot.slow", "0" };
+static settings::Int miss_chance{ "aimbot.miss-chance", "0" };
+
+static settings::Boolean projectile_aimbot{ "aimbot.projectile.enable", "true" };
+static settings::Float proj_gravity{ "aimbot.projectile.gravity", "0" };
+static settings::Float proj_speed{ "aimbot.projectile.speed", "0" };
+static settings::Float proj_start_vel{ "aimbot.projectile.initial-velocity", "0" };
+
+static settings::Float sticky_autoshoot{ "aimbot.projectile.sticky-autoshoot", "0.5" };
+
+static settings::Boolean aimbot_debug{ "aimbot.debug", "0" };
+
+static settings::Boolean auto_spin_up{ "aimbot.auto.spin-up", "0" };
+static settings::Boolean minigun_tapfire{ "aimbot.auto.tapfire", "false" };
+static settings::Boolean auto_zoom{ "aimbot.auto.zoom", "0" };
+static settings::Boolean auto_unzoom{ "aimbot.auto.unzoom", "0" };
+
+static settings::Boolean backtrackAimbot{ "aimbot.backtrack", "0" };
+static settings::Boolean backtrackLastTickOnly("aimbot.backtrack.only-last-tick", "true");
+static bool force_backtrack_aimbot = false;
+static settings::Boolean backtrackVischeckAll{ "aimbot.backtrack.vischeck-all", "0" };
+
+// TODO maybe these should be moved into "Targeting"
+static settings::Float max_range{ "aimbot.target.max-range", "4096" };
+static settings::Boolean ignore_vaccinator{ "aimbot.target.ignore-vaccinator", "1" };
+static settings::Boolean ignore_deadringer{ "aimbot.target.ignore-deadringer", "1" };
+static settings::Boolean buildings_sentry{ "aimbot.target.sentry", "1" };
+static settings::Boolean buildings_other{ "aimbot.target.other-buildings", "1" };
+static settings::Boolean npcs{ "aimbot.target.npcs", "1" };
+static settings::Boolean stickybot{ "aimbot.target.stickybomb", "0" };
+static settings::Boolean rageonly{ "aimbot.target.ignore-non-rage", "0" };
+static settings::Int teammates{ "aimbot.target.teammates", "0" };
+
+/*
+ * 0 Always on
+ * 1 Disable if being spectated in first person
+ * 2 Disable if being spectated
+ */
+static settings::Int specmode("aimbot.spectator-mode", "0");
+static settings::Boolean specenable("aimbot.spectator.enable", "0");
+static settings::Float specfov("aimbot.spectator.fov", "0");
+static settings::Int specslow("aimbot.spectator.slow", "0");
+
+settings::Boolean engine_projpred{ "aimbot.debug.engine-pp", "1" };
 
 int slow_aim;
 float fov;
@@ -327,17 +392,17 @@ static void CreateMove()
     projectile_mode          = false;
     projectileAimbotRequired = false;
     bool should_zoom = *auto_zoom;
-
     switch(get_weapon_mode){
         case weapon_hitscan:{
-            if(should_zoom)
-             doAutoZoom(false);
             if(should_backtrack)
                 updateShouldBacktrack(); 
             CachedEntity *target_entity = target_last = RetrieveBestTarget(aimkey_status);
             if(small_box_checker(target_entity)){
+                    if(should_zoom)
+                        doAutoZoom(true);
                     int weapon_case = g_pLocalPlayer->weapon()->m_iClassID();
                     Aim(target_entity);
+                     
                     if(!hitscan_special_cases(target_entity, weapon_case))
                         DoAutoshoot();
             }
@@ -346,14 +411,14 @@ static void CreateMove()
         }
         case weapon_melee:{
         CachedEntity *target_entity = target_last = RetrieveBestTarget(aimkey_status);    
-        DoAutoshoot();
-            if (g_pLocalPlayer->weapon_melee_damage_tick){
+        
                  if(should_backtrack)
                     updateShouldBacktrack(); 
                 
                 if(small_box_checker(target_entity))
                     Aim(target_entity);
-            }
+                    DoAutoshoot();
+            
         break; 
         }   
         case weapon_projectile:
@@ -384,12 +449,8 @@ static void CreateMove()
         break;
         }
 
-
-
     }
 
-       
-    
 }
 bool projectile_special_cases(CachedEntity* target_entity, int weapon_case){
 
@@ -491,9 +552,9 @@ switch(weapon_case){
 
 }
 bool small_box_checker(CachedEntity* target_entity){
-    if (!g_IEntityList->GetClientEntity(target_entity->m_IDX) || CE_BAD(target_entity))
+    if (CE_BAD(target_entity) ||!g_IEntityList->GetClientEntity(target_entity->m_IDX))
         return false;
-    else if (!target_entity->hitboxes.GetHitbox(calculated_data_array[target_entity->m_IDX].hitbox))
+    if (!target_entity->hitboxes.GetHitbox(calculated_data_array[target_entity->m_IDX].hitbox))
         return false;
     #if ENABLE_VISUALS
     if (target_entity->m_Type() == ENTITY_PLAYER)
