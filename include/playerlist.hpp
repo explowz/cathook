0834d53718117bfa186e5ae4d47473f64fd141a6
Bintr <20655963+explowz@users.noreply.github.com>
2023-09-11 20:50:46 +0300

    Replace `boost::unordered_flat_map` with `std::unordered_map`

--- include/playerlist.hpp
+++ include/playerlist.hpp
@@ -49,7 +49,7 @@ struct userdata
     unsigned kills{ 0 };
 };
 
-extern boost::unordered_flat_map<unsigned, userdata> data;
+extern std::unordered_map<unsigned, userdata> data;
 
 void Save();
 void Load();
