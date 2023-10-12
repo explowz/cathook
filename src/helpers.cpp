eef6c24229f79953b60524e786e4b7fc2f7bce5a
Bintr <20655963+explowz@users.noreply.github.com>
2023-10-12 20:17:13 +0300

    Fix multiple definition of `split` errors

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -28,6 +28,35 @@ std::vector<ConCommand *> &RegisteredCommandsList()
     return list;
 }
 
+std::vector<std::string> split(const std::string &s, char delimiter)
+{
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(s);
+    while (std::getline(tokenStream, token, delimiter))
+    {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+std::vector<std::string> split(const std::string &s, const std::string &delimiters)
+{
+    std::vector<std::string> tokens;
+    size_t pos_start = 0, pos_end;
+    const size_t delim_len = 1;
+    std::string token;
+
+    while ((pos_end = s.find_first_of(delimiters, pos_start)) != std::string::npos)
+    {
+        tokens.push_back(s.substr(pos_start, pos_end - pos_start));
+        pos_start = pos_end + delim_len;
+    }
+
+    tokens.push_back(s.substr(pos_start));
+    return tokens;
+}
+
 void BeginConVars()
 {
     logging::Info("Begin ConVars");
